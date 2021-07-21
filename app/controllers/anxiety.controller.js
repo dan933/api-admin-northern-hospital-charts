691185c3bbb5bc6d093c801564a2df179e81fc10
dan933 <69749413+dan933@users.noreply.github.com>
2021-07-21 14:14:08 +1000

    Pagination removed from the anxiety end point this endpoint will be used to get data for line graphs the data will be filtered based on a date range.

--- app/controllers/anxiety.controller.js
+++ app/controllers/anxiety.controller.js
@@ -10,21 +10,20 @@ const Op = db.Sequelize.Op;
 
 exports.find = ( req, res) => {
 
-    const { page, size } = req.query;
-    const { limit, offset } = pagination.getPagination( page, size );
+    const sort = req.params.sort;
+    const ascDesc = req.params.ascDesc === 'true' ? 'ASC':'DESC';
 
     const { startDate } = req.query;
 
     const id = req.params.id;
-    console.log(id)
 
-    const endDate = "2020-07-30"
+    const endDate = "2050-07-30"
     // const endDate = new Date (Date.now());
     // console.log(endDate.toISOString())
 
     const columnArray = ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'questionare_date']
 
-    Anxiety.findAndCountAll({
+    Anxiety.findAll({
         attributes:columnArray,
         where: {
             [Op.and]:[
@@ -33,12 +32,12 @@ exports.find = ( req, res) => {
             ]
                        
         },
-        limit,
-        offset
+        order:[
+            [sort,ascDesc]
+        ],
     })
     .then(data => {
-        const response = pagination.getPagingData(data, page, limit);
-        res.send(response);
+        res.send(data);
     })
     .catch(err => {
         res.status(500).send({
--- app/routes/anxiety.routes.js
+++ app/routes/anxiety.routes.js
@@ -3,8 +3,8 @@ const anxiety = require("../controllers/anxiety.controller.js")
 let router = require("express").Router();
 
 //Get an overview of all patients
-router.get("/find/id/:id", anxiety.find);
+router.get("/find/:sort/:ascDesc/id/:id", anxiety.find);
 
-// http://localhost:3000/api/anxietydepression/find/id/1?page=0&size=10
+// http://localhost:3000/api/anxietydepression/find/questionare_date/true/id/7?startDate=2020-06-18T00:50:12.000Z&page=0&size=10
 
 module.exports = router;
