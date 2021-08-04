c254b37fd464d38e2e0af610e6b472bd64497072
dan933 <69749413+dan933@users.noreply.github.com>
2021-08-04 16:15:01 +1000

    date filter changes anxiety controller

--- app/controllers/anxiety.controller.js
+++ app/controllers/anxiety.controller.js
@@ -13,14 +13,10 @@ exports.find = ( req, res) => {
     const sort = req.params.sort;
     const ascDesc = req.params.ascDesc === 'true' ? 'ASC':'DESC';
 
-    const { startDate } = req.query;
+    const { startDate, endDate } = req.query;
 
     const id = req.params.id;
-
-    const endDate = "2025-06-09"
-    // const endDate = new Date (Date.now());
-    // console.log(endDate.toISOString())
-
+   
     //select replaced by everycolumn
     const columnArray = ['d1','d2','d3','d4','d5','d6','d7','d8','a1','a2','a3','a4','a5','a6','a7','a8','questionare_date']
 
