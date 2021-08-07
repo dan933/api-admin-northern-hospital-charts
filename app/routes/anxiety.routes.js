43608fddc6565ee3469feb27edf4378f5f22c45f
dan933 <69749413+dan933@users.noreply.github.com>
2021-08-07 14:18:20 +1000

    download excel file endpoint successfully created

--- app/routes/anxiety.routes.js
+++ app/routes/anxiety.routes.js
@@ -10,6 +10,6 @@ router.get("/filter/:sort/:ascDesc/id/:id", anxiety.filter);
 http://localhost:3000/api/anxietydepression/filter/questionare_date/false/id/3?startDate=Thu%20Aug%2006%202020%2015:52:54%20GMT+1000%20(Australian%20Eastern%20Standard%20Time)&endDate=Fri%20Aug%2006%202021%2015:52:54%20GMT+1000%20(Australian%20Eastern%20Standard%20Time)&searchd1=&searchd2=&searchd3=&searchd4=&searchd5=&searchd6=&searchd7=&searchd8=&searcha1=&searcha2=&searcha3=&searcha4=&searcha5=&searcha6=&searcha7=&searcha8=&page=0&size=10
 
 router.get("/download/:id", anxiety.download);
-//http://localhost:3000/api/anxietydepression/download/3?startDate=2019-01-01&endDate=2025-01-01
+//http://localhost:3000/api/anxietydepression/download/3?startDate=2021-06-16&endDate=2021-06-17
 
 module.exports = router;
